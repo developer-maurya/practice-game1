9f10b835dadcc349a63db07b5dfca3154e572cc9
Chitranjan kumar <chitranjan75075@gmail.com>
2025-09-09 23:29:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset-btn");
+
+let turn0 = true;
+let arr =["apple", "banana", "litchi"]
+
+let arr2 = [["apple", "litchi"], ["patato", "mushuroom"],["pants","shirt"]]
